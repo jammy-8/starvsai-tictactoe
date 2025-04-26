29935a254102173a9cdaadac8e0a8effe6737ac7
jammy-8 <unb1eoh2@students.codeinstitute.net>
2025-04-26 10:38:27 +0100

    turns of players

--- main.py
+++ main.py
@@ -6,5 +6,19 @@ for i in range (3):
     grid.append(line)
     line = []
 
-
+def print_grid():
+    for i in range(3):
+        print("|", end ="")
+        for j in range(3):
+            print (grid[i][j], "|", end ="")
+        print("")
+        
+def player_turn(turn_player1):
+    if turn_player1 == True:
+        turn_player1 = False
+        print(f"It's {player2}'s turn")
+    else:
+        turn_player1 = True
+        print(f"It's {player1}'s turn")        
+    return turn_player1
 
